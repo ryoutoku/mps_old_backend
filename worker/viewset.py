87a5db9c2af6dfd31b307823f6236ec423c0b4ff
Akinori Hayashi <invisiblechain@hotmail.co.jp>
2019-01-31 20:41:07 +0900

    fix: APIの実装統一

--- worker/viewset.py
+++ worker/viewset.py
@@ -1,44 +1,79 @@
 import django_filters
 from rest_framework import viewsets, filters, mixins
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, BasePermission
 from rest_framework.response import Response
+from rest_framework import generics
 
 from .models import Worker, Resume, WorkerBank
 from .selializer import WorkerSerializer, ResumeSerializer, BankSerializer
 
 
-class WorkerViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    authentication_classes = (SessionAuthentication, )
-    permission_classes = (IsAuthenticated,)
+class IsWorker(BasePermission):
+    def has_permission(self, request, view):
+        return True
+        # return (request.user is not None) & hasattr(request.user, "worker")
+
+
+class WorkerViewSet(viewsets.GenericViewSet,
+                    mixins.CreateModelMixin, mixins.ListModelMixin,):
+
+    authentication_classes = (SessionAuthentication,)
+    permission_classes = (IsAuthenticated, IsWorker)
 
     queryset = Worker.objects.all()
     serializer_class = WorkerSerializer
 
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.queryset.filter(pk=request.user.worker.id).update(
+            **serializer.data)
+        return Response(serializer.data)
+
     def list(self, request):
-        model = request.user
-        serializer = self.serializer_class(
-            self.queryset.filter(pk=model.worker.id).first())
+        serializer = self.get_serializer_class()(
+            self.queryset.filter(pk=request.user.worker.id).first())
         return Response(serializer.data)
 
 
-class BankViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class BankViewSet(viewsets.GenericViewSet,
+                  mixins.CreateModelMixin, mixins.ListModelMixin,):
     authentication_classes = (SessionAuthentication, )
     permission_classes = (IsAuthenticated,)
 
     queryset = WorkerBank.objects.all()
     serializer_class = BankSerializer
 
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.queryset.filter(pk=request.user.worker.bank.id).update(
+            **serializer.data)
+        return Response(serializer.data)
+
     def list(self, request):
-        model = request.user
-        serializer = self.serializer_class(
-            self.queryset.filter(pk=model.worker.bank.id).first())
+        serializer = self.get_serializer_class()(
+            self.queryset.filter(pk=request.user.worker.bank.id).first())
         return Response(serializer.data)
 
 
-class ResumeViewSet(viewsets.ModelViewSet):
+class ResumeViewSet(viewsets.GenericViewSet,
+                    mixins.CreateModelMixin, mixins.ListModelMixin,):
     authentication_classes = (SessionAuthentication, )
     permission_classes = (IsAuthenticated,)
 
     queryset = Resume.objects.all()
     serializer_class = ResumeSerializer
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.queryset.filter(pk=request.user.worker.bank.id).update(
+            **serializer.data)
+        return Response(serializer.data)
+
+    def list(self, request):
+        serializer = self.get_serializer_class()(
+            self.queryset.filter(pk=request.user.worker.bank.id).first())
+        return Response(serializer.data)
