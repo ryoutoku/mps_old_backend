f0253dc47c3e77528210a09c6cd0087f9051e97b
Akinori Hayashi <invisiblechain@hotmail.co.jp>
2019-02-02 23:45:41 +0900

    ref: 不要なimport削除

--- worker/urls.py
+++ worker/urls.py
@@ -1,9 +1,6 @@
 # coding: utf-8
 from rest_framework.routers import DefaultRouter
-from rest_framework.urlpatterns import format_suffix_patterns
-from django.urls import path
 from .viewset import WorkerViewSet, ResumeViewSet, BankViewSet
-from django.conf.urls import url
 
 from utility.router import SameURLRouter
 
