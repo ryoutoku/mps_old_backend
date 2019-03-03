28a44ecce914f1c2fd5ffa552bea306dfa3b238d
ryoutoku <invisible060@gmail.com>
2019-03-03 12:34:53 +0900

    fix: 必須入力完了チェックロジック修正

--- worker/selializer.py
+++ worker/selializer.py
@@ -11,7 +11,7 @@ class WorkerBasicInfoSerializer(serializers.ModelSerializer):
         model = WorkerBasicInfo
 
         fields = (
-            "is_activate",
+            "is_activated",
             "last_name",
             "last_name_kana",
             "first_name",
@@ -26,10 +26,24 @@ class WorkerBasicInfoSerializer(serializers.ModelSerializer):
             "bank_account_number",
         )
 
-    def validate_is_activate(self, is_activate):
-        """activateはシリアライズ時にはチェックしない
-        """
-        return is_activate
+    def validate(self, attrs):
+        last_name = attrs["last_name"]
+        first_name = attrs["first_name"]
+        last_name_kana = attrs["last_name_kana"]
+        first_name_kana = attrs["first_name_kana"]
+        address = attrs["address"]
+
+        is_activated = True
+
+        if last_name is None or \
+                last_name_kana is None or \
+                first_name is None or \
+                first_name_kana is None or \
+                address is None:
+            is_activated = False
+        attrs["is_activated"] = is_activated
+
+        return attrs
 
 
 class WorkerConditionSerializer(serializers.ModelSerializer):
